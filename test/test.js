57fd7eba6d6efd7f3ae0b57eed6bcaa90c61cbc1
Matthew Palmer <matt@palmer.im>
2013-03-23 22:09:51 +1100

    cleaned up test.js

--- test/test.js
+++ test/test.js
@@ -5,20 +5,6 @@ var subtract = require('../subtract');
 var multiply = require('../multiply');
 var divide = require('../divide');
 var sorter = require('../sort');
-/*
-describe('Array', function() {
-	describe('#indexOf',function() {
-		it('should return -1 when the value is not present',function() {
-			assert.equal(-1, [1,2,3].indexOf(5));
-			assert.equal(-1, [1,2,3].indexOf(0));
-		});
-		it('should return the index when the value is present',function() {
-			assert.equal(2, [1,2,3].indexOf(3));
-		});
-
-	});
-});
-*/
 
 //CALCULATOR TESTS - should each of these go in a new file?
 describe('add',function() {
